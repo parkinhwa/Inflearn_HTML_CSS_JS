3218df8e2eba2e7d488c6647dc35ac3648a7093c
parkinhwa <65644486+parkinhwa@users.noreply.github.com>
2021-12-08 18:37:24 +0900

    기본구문, 선택자

---
+++ Session02/JavaScript/1-기본구문_선택자.js
@@ -0,0 +1,10 @@
+function show() {
+  var x = document.getElementById("abc");
+  x.style.fontSize = "16px";
+  x.style.color = "black";
+}
+function hide() {
+  var x = document.getElementById("abc");
+  x.style.color = "white";
+  x.style.fontSize = "16px";
+}
